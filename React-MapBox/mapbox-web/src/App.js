cd0c129476d7c1cc4891f75b57b2b8f763bd57bd
Nico-Kasz <nicolas.kaszynski@gmail.com>
2022-02-06 18:10:14 -0500

    stable but not very functional

--- React-MapBox/mapbox-web/src/App.js
+++ React-MapBox/mapbox-web/src/App.js
@@ -1,38 +1,32 @@
 import * as React from 'react';
 import { useState } from 'react';
 import ReactMapGL, { Marker } from 'react-map-gl';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import pin from "./icons/pin.png";
+
+const api = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
+const token = 'pk.eyJ1Ijoibmljb2thc3oiLCJhIjoiY2t6MGN4djV5MWFuczJwcDFwYW0zMWxhZyJ9.aOdFpJtgU4ow7ascIJDXQA';
 
 
-function getForwardGeocoding(search_text) {
-    console.log(encodeURI('https://api.mapbox.com/geocoding/v5/mapbox.places/' + search_text + '.json?access_token=pk.eyJ1Ijoibmljb2thc3oiLCJhIjoiY2t6MGN4djV5MWFuczJwcDFwYW0zMWxhZyJ9.aOdFpJtgU4ow7ascIJDXQA'));
-    return fetch(encodeURI('https://api.mapbox.com/geocoding/v5/mapbox.places/' + search_text + '.json?access_token=pk.eyJ1Ijoibmljb2thc3oiLCJhIjoiY2t6MGN4djV5MWFuczJwcDFwYW0zMWxhZyJ9.aOdFpJtgU4ow7ascIJDXQA'))
-        .then((response) => response.json())
-        .then((responseJson) => {
-            return responseJson;
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+
+async function getForwardGeocoding(search_text) {
+    const response = await fetch(api + search_text + '.json?access_token=' + token);
+    const data = await response.json();
+    makeMarker(data.features[0].center[0], data.features[0].center[1]);
+    console.log(data.features[0].center);
+    return data;
 }
 
-async function jsonData(search_text) { // (1)
-    let res = await fetch(encodeURI('https://api.mapbox.com/geocoding/v5/mapbox.places/' + search_text + '.json?access_token=pk.eyJ1Ijoibmljb2thc3oiLCJhIjoiY2t6MGN4djV5MWFuczJwcDFwYW0zMWxhZyJ9.aOdFpJtgU4ow7ascIJDXQA')); // (2)
-
-    if (res.status === 200) {
-        let json = await res.json(); // (3)
-        return json;
-    }
-
-    throw new Error(res.status);
-}
 
 function makeMarker(long, lat) {
-    return (
-        <Marker longitude={long} latitude={lat} anchor="bottom" draggable={true} >
-            <img src="pin.png" alt="pin"/>
+    return (            // Need to fix these variables   VV anchor and VV scale arent applying properties in map rendering
+        <Marker longitude={long} latitude={lat} anchor="bottom" Scale=".2">
+            <img src={pin} alt="pin" />
         </Marker>
     );
 }
+
 
 export default function Map() {
     const [viewport, setViewport] = useState({
@@ -41,29 +35,20 @@ export default function Map() {
         latitude: 39.50882818527073,
         longitude: -84.73455522976074,
         zoom: 14
-        //style: 'mapbox://styles/mapbox/streets-v11'
     });
 
-    var o = jsonData('241 Eisenhower Way').then();
-    console.log(o);
-  
-
-    //var obj = getForwardGeocoding('9481 Chesapeake Drive').then(responseJson => {return responseJson});
-    //console.log(obj);
-    //var myObject = JSON.parse(obj);
-    //console.log(myObject);
-
     return (
-        
-        <ReactMapGL
-            {...viewport}
-            mapboxApiAccessToken="pk.eyJ1Ijoibmljb2thc3oiLCJhIjoiY2t6MjE2NXprMDF4
-czJ2b21uZjhqOXlhaCJ9.pzhG-dabniu4rtlDnkIVjw"
-            mapStyle="mapbox://styles/nicokasz/ckz23hv99001w14qmii9m7ac4"
-            onViewportChange={(viewport) => { setViewport(viewport); }}
-            >
-                {makeMarker( -85, 40)}
-
+        <div>
+                    <ReactMapGL
+                        {...viewport}
+                        mapboxApiAccessToken={token}
+                    mapStyle="mapbox://styles/nicokasz/ckz23hv99001w14qmii9m7ac4"
+                    onViewportChange={(viewport) => { setViewport(viewport); }}
+                    >
+            
+                    {makeMarker(-85, 40)}
+            
             </ReactMapGL>
+        </div>
         );
 } 
