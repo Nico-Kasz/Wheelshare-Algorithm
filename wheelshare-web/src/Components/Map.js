c5614d6adb2ab175333eaeb18bccd26361704830
Nico Kasz <nicolas.kaszynski@gmail.com>
2022-05-19 14:03:16 -0400

    Converted Pin to tsx component

--- wheelshare-web/src/Components/Map.js
+++ wheelshare-web/src/Components/Map.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useState} from 'react';
 import ReactMapGL, { GeolocateControl, Marker, Source, Layer } from 'react-map-gl';
 import {heatmapLayer} from './heatmap';
-import Pin from './Pin';
+import Pin from './Pin.tsx';
 
 // Constants Imports
 import { API, TOKEN } from "./constants";
@@ -14,7 +14,7 @@ var heatmapOn = false;
 const heatmapData =  require('../Assets/Geojsons/4.20.22 gps data.geojson');
 
 // {name: _, address: _, longitude: _, latitude: _}
-const Markers = [{name: "StartMarker"}, {name: "EndMarker"}, {latitude: 39.50882818527073, longitude: -84.73455522976074, name: "TestMarker"}];
+const Markers = [{name: "StartMarker", address: "Billy ave"}, {name: "EndMarker"}, {latitude: 39.50882818527073, longitude: -84.73455522976074, name: "TestMarker"}];
 
 const setStartAddress   = (address) => {Markers[0].address = address;}
 const setEndAddress     = (address) => {Markers[1].address = address;}
@@ -30,20 +30,21 @@ const DisplayHeatmap = () => {
         )
 }
 
-
+// Might have to have for let i, then use map to display all 
 const DisplayMarkers = () => {
     return Markers.map(function (marker) {
+        console.log(marker);
         if (!('latitude' in marker && 'longitude' in marker)) {
-            if ('address' in marker)
+            if ('address' in marker) {
                 // Forward Geocode - TODO - Async Wait => set marker
                 //marker = GetLocationByAddress(marker.address).then();
-                ;
-            else
+                marker = GetLocationByAddress(marker.address, marker.name);
+            } else
                 // Blank marker - nothing to display
                 ;
-        }
-        //Since GeoCoding doesnt work as of now, a return is put in place to prevent crashing
-        
+        } 
+        //Since GeoCoding doesnt work as of now, the marker data is logged
+        console.log(marker);
 
 
         // Marker on drag event => moves to location it is dropped at - TODO - throws exception
@@ -51,18 +52,19 @@ const DisplayMarkers = () => {
             marker = {name: marker.name, address: marker.address, longitude: ev.lngLat[0], latitude: ev.lngLat[1]};
             //marker.longitude = ev.lngLat[0];
             //marker.latitude =  ev.lngLat[1];
+            //console.log(ev);
         }
 
         return(
             <Marker 
                 className={'name' in marker ? marker.name : "Marker"}
-                longitude={'longitude' in marker? marker.longitude : 0}  // The in statements are placeholders
+                longitude={'longitude' in marker? marker.longitude : 0}  // The "in" statements are placeholders
                 latitude={'latitude' in marker? marker.latitude : 0} 
                 anchor="bottom"
-                key = {marker.name}
+                key = {marker.name}         // Make this unique TODO
                 pitchAlignment= 'map'
                 draggable={true}
-                onDragEnd={ ev => marker = markerOnDragEvent(marker, ev)}>
+                onDragEnd={ ev => markerOnDragEvent(ev)}>
                 <Pin/> 
             </Marker>)
         }
@@ -70,13 +72,16 @@ const DisplayMarkers = () => {
 
 }
 
-const GetLocationByAddress  = (address, name = "Marker") => {
-    fetch(API + encodeURIComponent(address) + '.json?access_token=' + TOKEN)
-            .then(resp => resp.json())
-            .then(json => {
-                if (json.features[0] !== undefined)           // Just incase
-                    return {name: name, address: address, longitude: json.features[0].center[0], latitude: json.features[0].center[1]};
-            })
+const GetLocationByAddress = (address, name) => {
+    let marker = 
+        fetch(API + encodeURIComponent(address) + '.json?access_token=' + TOKEN)
+                .then(resp => resp.json())
+                .then(json => {
+                    console.log(json);
+                    if (json.features[0] !== undefined)           // Just incase
+                        return {name: name===undefined? "Marker" : name, address: address, longitude: json.features[0].center[0], latitude: json.features[0].center[1]};
+                })
+    return marker;
 }
 
 
