a6eb45b9e50afdeafb9ec3e5ebbe2b3dfb5bcf02
Nico Kasz <nicolas.kaszynski@gmail.com>
2022-06-22 13:15:03 -0400

    Recreated Feedback UI

--- React-MapBox/src/Components/MapUI.js
+++ React-MapBox/src/Components/MapUI.js
@@ -4,6 +4,8 @@ import { setAddress } from "./Map";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../Assets/CSS/MapUI.css";
 
+const AccountInfo = { loggedIn: false };
+
 function handleSubmit(event) {
   // Sanitation happens before fetching
   setAddress(0, event.target[0].value);
@@ -22,6 +24,23 @@ function swapSearchTerms() {
   fromRef.value = toRef.value;
   toRef.value = temp;
 }
+
+const DisplayProfile = () => {
+  if (AccountInfo.loggedIn) {
+    return <div id="Account"></div>;
+  } else {
+    return (
+      <div id="SignIn">
+        <Row>
+          <Button className="Account-Button">Log In</Button>
+        </Row>
+        <Row>
+          <Button className="Account-Button">Sign Up</Button>
+        </Row>
+      </div>
+    );
+  }
+};
 
 export default function UI() {
   // TODO - Create multiple Menus and ability to switch between them
@@ -73,31 +92,24 @@ export default function UI() {
               </div>
             </Form>
           </Container>
-          <div id="Slider" >
-          <div className="Slider-Text">Slope Tolerance:</div>
-          <Row>
-            <input
-              id="InclineSlider"
-              className="Slider"
-              type="range"
-              min="0"
-              max="15"
-              defaultValue="3"
-              step=".2"
-              onChange={(event) => {
-                console.log(event.target.value);
-              }}
-            />
-          </Row>
+          <div id="Slider">
+            <div className="Slider-Text">Slope Tolerance:</div>
+            <Row>
+              <input
+                id="InclineSlider"
+                className="Slider"
+                type="range"
+                min="0"
+                max="15"
+                defaultValue="3"
+                step=".2"
+                onChange={(event) => {
+                  console.log(event.target.value);
+                }}
+              />
+            </Row>
           </div>
-          <div id="Profile">
-          <Row>
-            <Button className="Account-Button">Log In</Button>
-          </Row>
-          <Row>
-            <Button className="Account-Button">Sign Up</Button>
-          </Row>
-          </div>
+          <div id="Profile">{DisplayProfile()}</div>
           <div id="Footer-Info">
             <Row>
               <Col xs={4}>
@@ -109,7 +121,16 @@ export default function UI() {
                   About
                 </a>
               </Col>
-              <Col xs={4}>Feedback</Col>
+              <Col xs={4}>
+                <Button
+                  onClick={() => {
+                    let Panel = document.getElementById("Feedback");
+                    Panel.classList.remove("Feedback-Hidden");
+                  }}
+                >
+                  Feedback
+                </Button>
+              </Col>
               <Col xs={4}>
                 <a href="mailto:raychov@MiamiOH.edu">Contact Us</a>
               </Col>
@@ -134,6 +155,37 @@ export default function UI() {
             &lt;
           </p>
         </Button>
+        <div id="Feedback" className="Feedback Feedback-Hidden">
+          <Form
+            onSubmit={(event) => {
+              event.preventDefault();
+              console.log(event.target[0].value);
+              let Panel = document.getElementById("Feedback");
+              Panel.classList.add("Feedback-Hidden");
+            }}
+          >
+            <textarea className="form-control Feedback-Text" rows={6} />
+            <Row>
+              <Col xs={6}>
+                <Button
+                  className="Feedback-Button"
+                  onClick={() => {
+                    let Panel = document.getElementById("Feedback");
+                    Panel.classList.add("Feedback-Hidden");
+                  }}
+                >
+                  {" "}
+                  Close
+                </Button>
+              </Col>
+              <Col xs={6}>
+                <Button type="submit" className="Feedback-Button">
+                  Send Feedback
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </div>
       </div>
     </div>
   );
